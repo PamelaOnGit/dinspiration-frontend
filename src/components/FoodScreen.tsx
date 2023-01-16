7f202c8a554fda267ec314a69d40fcaddf439095
Pamela Smith <pammyokeeffe@gmail.com>
2023-01-16 14:25:55 +0000

    filtered inspirations show in console on the foodScreen page (not yet rendered in JSX)

--- src/components/FoodScreen.tsx
+++ src/components/FoodScreen.tsx
@@ -11,19 +11,17 @@ import { baseUrl } from "../config"
 
 type Food = IFood
 
-//
-
 interface IInspiration {
-	_id: string,
-	name: string,
+  _id: string,
+  name: string,
 	recipeURL: string,
-	description: string,
-	userImage: string,
+  description: string,
+  userImage: string,
 	userId: string,
 	primaryFood: string,
-	secondaryFood: string
+	secondaryfood: string
+  }
 
-}
 
 type Inspirations = null | Array<IInspiration>
 
@@ -32,70 +30,38 @@ type Inspirations = null | Array<IInspiration>
 function FoodScreen(){
 
 
-  const {id} = useParams()
+  const  { id } = useParams()
   console.log(id)
 
   const [FoodDetails, showFoodDetails ] = React.useState<Food>()
-  // console.log(FoodDetails)
-  const arrayOptions = FoodDetails?.options
-  // console.log(arrayOptions)
 
-  const [inspirationsArray, updateInspirations] = React.useState<Inspirations>(null)
+  const [inspirations, setInspirations] = React.useState<Array<Inspirations> | null>(null)
+  console.log(inspirations)
+  
   React.useEffect(() => { 
 
     async function fetchInspirations() { 
 
+  //! In case helpful - this is the code we can use in order to make a get request 
+  //! WITH THE AUTH TOKEN ATTACHED 
+  //! which will allow us to filter foods according to the user
   const token = localStorage.getItem('token')
   console.log(token)
 
       const resp = await axios.get(`${baseUrl}/inspirations`, { headers: {"Authorization": `Bearer ${token}` } })
-      const InspirationData = await resp.data
-      updateInspirations(InspirationData)
-      console.log(InspirationData)
+      const inspirationsData = await resp.data
+      setInspirations(inspirationsData)
+      
     }
+//!
+
 
     fetchInspirations()
   }, [])
 
-  const [addInspirationButton, goToInspirationForm] = React.useState(false)
-  console.log(addInspirationButton)
 
-  // function createInpiration(){
-    // const navigate = useNavigate()
-    // navigate("//add-inspiration/:foodId")
 
-    // async function handleSubmit(e: SyntheticEvent) {
-    //   e.preventDefault()
-    //   if (userOptions.length > 1) {
-    //   setUserOptions(
-    //     userOptions.filter(a => a !== "anything")
-    //   )
-    //   }
-    //   if (userOptions.length < 1) { 
-    //     setUserOptions(["anything"])
-    //   }
-    //   try { 
-    //     const token = localStorage.getItem('token')
-    //     const { data } = await axios.patch('/api/my-options', {
-    //       "userOptions": userOptions
-    //     }, {headers: {"Authorization": `Bearer ${token}`}}  )
-    //     navigate('/my-lifestyle')
-    //   } catch(err) { 
-    //     console.log(err)
-    //   }
-    // }
   
-  
-    function handleClick(e: any) {
-      e.preventDefault()
-      const buttonValue = e.target.value
-      console.log(buttonValue)
-    }
-  
-  // console.log("From here, I can go to a link to create a new inspiration")
-  // }
-
-  // button broken for now. Jane will fix later
 
 
   React.useEffect(() => { 
@@ -130,19 +96,20 @@ fetchFoodDetails()
               {FoodDetails?.lifestyle.lowCarb && <p className="subtitle is-6">{"Low carb"}</p>}
               {FoodDetails?.lifestyle.lowGi && <p className="subtitle is-6">{"Low GI"}</p>}
         </div>
+        <Link to={`/add-inspiration/${id}`}><button className="button is-link">Add an inspiration</button></Link>
       </div>
-      <div className="card">
-					<div className="columns is-multiline">
-						{/* {inspirationsArray?.map((inspiration: Inspirations) => {
-							return <Inspiration
-								key={inspiration.name}
-								{...inspiration}
-							/>
-						})} */}
-					</div>
-      </div>
-      <Link to={`/add-inspiration/${id}`}><button className="button is-link">Add an inspiration</button></Link>
       
+      {/* <div className="container">
+      <div className="columns is-multiline">
+        {Inspirations?.map((inspiration: any) => {
+          return <Inspiration 
+            key={inspiration._id}
+            {...inspiration}
+          />
+        })}
+      </div> */}
+
+
   </div>
 }
 
